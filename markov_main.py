a7b282bf5ca4917cc30c1251c704e90c68bf099b
Yusuf Sezer <yusufksezer@hotmail.com>
2017-04-15 14:55:11 -0400

    Made small changes to documentation.

--- markov_main.py
+++ markov_main.py
@@ -37,20 +37,26 @@ for i in range(0, 15):
     lastID = last['id']
     List += [t.json()]
 
+'''
 # Flattens List so that we have one list of all the words from Trump's most-recent 3200 tweets
 # Filters out retweets, mentions and links
 # Fixes encoding issue where '&' are being read as '&amp'
 # Please excuse disgusting one-liner
+'''
 flattenedList = list(map(lambda x: "&" if x == "&amp;" else x, filter(lambda z: not "http" in z and not "RT" in z and not "@" in z, reduce(lambda x, y: x+" "+y, [elem['text'] for item in List for elem in item]).split())))
 
+'''
 # Randomly selects three words from Trump's Tweets to write to the beginning of the file
 # Our Markov Chain uses the first three words of the file to create the initial prefix-suffix pair
 # This ensures that each Markov-Generated Tweet starts with different words
 randomInt = randomizer(len(flattenedList) - 4)
+'''
 randomSelection = flattenedList[randomInt:randomInt+3]
 
+'''
 # Write our randomly selected 3 words to a .txt file
 # Then write all of the text from the Tweets we retrieved earlier
+'''
 with open("Output.txt", "w", encoding='utf-8') as text_file:
     for item in randomSelection:
         text_file.write(item + " ")
